da5cd85e2c2c7dc6d6b21158b9843e0d31c459c8
Freddy Montes <freddymontes@gmail.com>
2018-08-04 16:55:13 -0600

    Add library to get data

---
+++ src/lib/MyLib.js
@@ -0,0 +1,108 @@
+const MONTH_NAMES = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'Junio',
+    'Julio',
+    'August',
+    'September',
+    'Octuber',
+    'November',
+    'December',
+]
+
+function getDayModel(day) {
+    const dayOfTheWeek = day.getDay()
+
+    return {
+        day: dayOfTheWeek,
+        holiday: false, // Will set this later
+        number: day.getDate(),
+        weekend: dayOfTheWeek === 0 || dayOfTheWeek === 6,
+    }
+}
+
+function getEndDate(date, days) {
+    const result = new Date(date)
+    result.setDate(result.getDate() + days)
+    return result
+}
+
+function getModel(months) {
+    return months.map(month => {
+        return {
+            name: getMonthName(month[0]),
+            weeks: splitIntoWeeks(month),
+        }
+    })
+}
+
+function getMonthName(month) {
+    return MONTH_NAMES[month.getMonth()] + ' ' + month.getFullYear()
+}
+
+function getRange(start, end) {
+    const dates = [start]
+    const month = start.getMonth()
+    const year = start.getFullYear()
+    let day = start.getDate()
+
+    while (dates[dates.length - 1] < end) {
+        dates.push(new Date(year, month, ++day))
+    }
+
+    return dates
+}
+
+function splitMonths(range) {
+    let prevMonth
+    let currentMonth = 0
+    const result = []
+
+    range.forEach(date => {
+        if (prevMonth === undefined) {
+            prevMonth = date.getMonth()
+            result[currentMonth] = []
+        }
+
+        if (prevMonth !== date.getMonth()) {
+            currentMonth++
+            prevMonth = date.getMonth()
+            result[currentMonth] = []
+        }
+
+        result[currentMonth].push(date)
+    })
+
+    return result
+}
+
+function splitIntoWeeks(month) {
+    const result = []
+    let currentWeek = 0
+
+    month.forEach(day => {
+        if (!result[currentWeek]) {
+            result[currentWeek] = []
+        }
+
+        result[currentWeek].push(getDayModel(day))
+
+        if (day.getDay() === 6) {
+            currentWeek++
+        }
+    })
+
+    return result
+}
+
+export default (params) => {
+    const finalDate = getEndDate(params.date, params.days)
+    const range = getRange(params.date, finalDate)
+    const months = splitMonths(range)
+    return getModel(months)
+}
+
+
---
+++ src/lib/MyLib.test.js
@@ -0,0 +1,89 @@
+import getData from './MyLib'
+
+const mock = [
+    {
+        name: 'December 2018',
+        weeks: [
+            [
+                {
+                    day: 0,
+                    holiday: false,
+                    number: 30,
+                    weekend: true,
+                },
+                {
+                    day: 1,
+                    holiday: false,
+                    number: 31,
+                    weekend: false,
+                },
+            ]
+        ],
+    },
+    {
+        name: 'January 2019',
+        weeks: [
+            [
+                {
+                    day: 2,
+                    holiday: false,
+                    number: 1,
+                    weekend: false,
+                },
+                {
+                    day: 3,
+                    holiday: false,
+                    number: 2,
+                    weekend: false,
+                },
+                {
+                    day: 4,
+                    holiday: false,
+                    number: 3,
+                    weekend: false,
+                },
+                {
+                    day: 5,
+                    holiday: false,
+                    number: 4,
+                    weekend: false,
+                },
+                {
+                    day: 6,
+                    holiday: false,
+                    number: 5,
+                    weekend: true,
+                },
+            ],
+            [
+                {
+                    day: 0,
+                    holiday: false,
+                    number: 6,
+                    weekend: true,
+                },
+                {
+                    day: 1,
+                    holiday: false,
+                    number: 7,
+                    weekend: false,
+                },
+                {
+                    day: 2,
+                    holiday: false,
+                    number: 8,
+                    weekend: false,
+                },
+            ]
+        ],
+    },
+]
+
+it('should get model', () => {
+    const model = getData({
+        date: new Date('12/30/2018'),
+        days: 9,
+        country: 'US'
+    });
+    expect(model).toEqual(mock);
+})
